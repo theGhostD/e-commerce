e6987d6783795d074b30b686422893e42a3b2fcd
ojeakale david <jordansmith4467@gmail.com>
2023-03-06 19:46:50 -0800

    modifications

--- src/component/payment_form/PaymentForm.jsx
+++ src/component/payment_form/PaymentForm.jsx
@@ -3,24 +3,24 @@ import "./payment.css"
 import { useStripe,useElements } from "@stripe/react-stripe-js"
 
 const PaymentForm = () => {
-    const stripe = useStripe();
-    const element = useElements()
+    // const stripe = useStripe();
+    // const element = useElements()
 
-    const paymentHandler = (e)=>{
-        e.preventDefault();
+    // const paymentHandler = (e)=>{
+    //     e.preventDefault();
 
-        if(!stripe || !element) {
-            return ;
-        }
+    //     if(!stripe || !element) {
+    //         return ;
+    //     }
 
-        const response = async ()=> fetch("")
-    }
+    //     const response = async ()=> fetch("")
+    // }
 
 
     return (
         <div className="formContainer">
             
-            <form onSubmit={paymentHandler}>
+            <form>
             <h2>credit card details</h2>
                 <CardElement className="ddd"/>
                 <button className="btn">pay now</button>
